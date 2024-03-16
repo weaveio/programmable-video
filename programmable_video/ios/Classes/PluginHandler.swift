b086daa2ea727ddb93da3bf9a6016387cf51b339
Rob Gagne <rob@fluxweave.com>
2024-03-16 21:40:49 +0900

    Remove isaac codec

--- programmable_video/ios/Classes/PluginHandler.swift
+++ programmable_video/ios/Classes/PluginHandler.swift
@@ -609,8 +609,6 @@ public class PluginHandler: BaseListener {
                 var audioCodecs: [AudioCodec] = []
                 for (_, audioCodec) in preferredAudioCodecs {
                     switch audioCodec {
-                    case "isac":
-                        audioCodecs.append(IsacCodec())
                     case "PCMA":
                         audioCodecs.append(PcmaCodec())
                     case "PCMU":
